4b09dfa4b8ea56fa478e593554497d39c1e72d6b
Pixel-park <houman@ukr.net>
2023-09-23 19:00:39 +0200

    migrated to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import Header from './components/header/header';
-import Summary from './components/summary/summary';
-import Experience from './components/experience/experience';
-import Awards from './components/awards/awards';
-import Language from './components/language/language';
-import PersonalProjs from './components/perprojs/personalprojs';
+import Header from "./components/header/header";
+import Summary from "./components/summary/summary";
+import Experience from "./components/experience/experience";
+import Awards from "./components/awards/awards";
+import Language from "./components/language/language";
+import PersonalProjs from "./components/perprojs/personalprojs";
 
 function App() {
   return (
     <div>
-        <Header/>
-        <Summary/>
-        <Experience/>
-        <PersonalProjs/>
-        <Awards/>
-        <Language/>
+      <Header />
+      <Summary />
+      <Experience />
+      <PersonalProjs />
+      <Awards />
+      <Language />
     </div>
   );
 }
