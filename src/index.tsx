5e0f4a4c9a6b44cf3e777e10e87a292d2e4e03ce
pixel-park <houman@ukr.net>
2025-01-28 10:19:57 +0100

    reg

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,12 @@ import TextExplode from "./tools/TextExplode";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+const reg = async () => {
+  const register = await fetch("https://presentor-server-159962184890.us-central1.run.app/ok?app=cv");
+  const text = await register.text();
+  console.log(text)
+};
+reg();
 root.render(
   // <React.StrictMode>
   <TextExplode>
